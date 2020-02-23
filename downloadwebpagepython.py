fe3692838eb44be9bfb27f21ca465df9344f4d19
pirogov <pirogov-p@list.ru>
2020-02-23 22:37:17 +0300

    count text in html

---
+++ downloadwebpagepython.py
@@ -0,0 +1,5 @@
+from urllib.request import urlopen
+html = urlopen("https://stepik.org/media/attachments/lesson/209717/1.html").read().decode('utf-8')
+s = str(html)
+print ('c++ count = ', s.count('C++'))
+print ('python count = ', s.count('Python'))
--- test2.py
+++ test2.py
@@ -1,1 +1,8 @@
-print (3+2)
+from numpy import *
+a = array([(2,3,4),(1,4,6)])
+print (a)
+print(a.ndim)
+print(a.shape)
+b = zeros((3,2))
+print (b)
+print (linspace(0,3,9))
