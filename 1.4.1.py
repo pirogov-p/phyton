164116aac1d978df6f48b29ef570d6cf5bb6910f
ppirogov <pirogov-p@list.ru>
2020-02-25 14:56:45 +0300

    1.4.1 video

---
+++ 1.4.1.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+page = requests.get(" https://stepik.org/media/attachments/lesson/209723/3.html")
+soup = BeautifulSoup(page.content, 'html.parser')
+table = soup.find('table')
+table_rows = table.find_all('tr')
+a=[]
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text for i in td]
+    a.append(row)
+sum = 0
+for b in a:
+    for c in b:
+        sum +=int(c)
+
+print (sum)
