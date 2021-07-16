9b5af2ea1bbcbd7b399f44544c618f54b68f373d
Павел <pirogov-p@list.ru>
2021-07-16 19:26:49 +0300

    create new project Tinkoff invest

---
+++ cgi-bin/test.py
@@ -0,0 +1,12 @@
+from openapi_client import openapi
+token = 't.b33Iid4Gn3-8JztmIXcRCFxQ8vsKl2wD_eYgb1PtyPtoyVSE6MYJ7EoYNNSHa9scLIXsVlmyCN0c6NmMBRicOw'
+client = openapi.api_client(token)
+# Получение списка облигаций
+bonds = client.market.market_bonds_get()
+# Получение списка ETF
+etfs = client.market.market_etfs_get()
+# Получение списка акций
+stocks = client.market.market_stocks_get()
+instr_list = bonds.payload.instruments + etfs.payload.instruments + stocks.payload.instruments
+
+instr_list[:3]
